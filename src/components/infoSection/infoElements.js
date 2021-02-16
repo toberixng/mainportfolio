a901b1de5786ac6d264e81e7406b3172443ed983
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2021-02-16 20:42:12 +0100

    update

--- src/components/infoSection/infoElements.js
+++ src/components/infoSection/infoElements.js
@@ -28,6 +28,47 @@ export const InfoRow = styled.div`
     grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'` : `'col1 col2'`)};
     
     @media screen and(max-width: 768px) {
-        grid-template-areas: ${({imgStart}) => (imgStart ? `'col1 col2'` : `'col1 col1'`)}
+        grid-template-areas: ${({ imgStart }) => (imgStart ? `'col1' 'col2'` : `'col1 col1'` `'col2 col2'`)}
     }
 `
+
+export const Column1 = styled.div`
+    margin-bottom: 15px;
+    padding: 0 15px;
+    grid-area: col1;
+`
+
+export const Column1 = styled.div`
+    margin-bottom: 15px;
+    padding: 0 15px;
+    grid-area: col2;
+`
+
+export const TextWrapper = styled.div`
+    max-width: 540px;
+    padding-top: 0;
+    padding-bottom: 60px;
+`
+
+export const TopLine = styled.p`
+    color: #01bf71;
+    font-size: 16px;
+    line-height: 16px;
+    font-weight: 700;
+    letter-spacing: 1.4px;
+    text-transform: uppercase;
+    margin-bottom: 16px;
+`
+
+export const Heading = styled.h1`
+    margin-bottom: 24px;
+    font-size: 48px;
+    line-height: 1.1;
+    font-weight: 600;
+    color: ${({ lightText }) => (lightText ? '#f7f8fa': '#010606')};
+
+    @media screen and (max-width: 480px) {
+        font-size: 32px;
+    }
+`
+
