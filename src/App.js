3656d14e6075ee937d2d252185a64d8bd727deae
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2021-06-08 20:06:53 +0100

    Total refactoring

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
-import Home from './pages';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer';
+import NavMenu from './components/NavMenu';
+import ScrollToTop from './components/ScrollToTop';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
 
-
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Home />
-    </Router>
+    <>
+      <Router>
+        <NavMenu />
+        <ScrollToTop />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
-
-export default App;
